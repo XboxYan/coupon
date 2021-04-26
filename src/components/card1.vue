6f24c214b49c844d2776a5ca0a6610e1b6c5eca5
严文彬 <yanwenbin@yuewen.com>
2021-04-26 20:50:11 +0800

    v-model

--- src/components/card1.vue
+++ src/components/card1.vue
@@ -1,24 +1,50 @@
 <template>
   <main class="main">
     <div class="card-con">
-      <div class="card"></div>
+      <div class="card" :style="style"></div>
     </div>
     <aside class="side">
       <section>
-        <lable>圆角</lable><input type="range" id="demo1_input" value="20">
+        <span>radius</span>
+        <input type="range" v-model="state.radius" />{{state.radius}}
       </section>
       <section>
-        <lable>方向</lable><input type="radio" name="demo1_radio" id="r1_1" checked>
-        <label for="r1_1">水平</label><input type="radio" name="demo1_radio" id="r1_2">
-        <label for="r1_2">垂直</label></section>
+        <span>direction</span>
+        <label><input type="radio" name="dir" value="horizontal" v-model="state.direction" />horizontal</label>
+        <label><input type="radio" name="dir" value="vertical" v-model="state.direction" />vertical</label>
+      </section>
       <section>
-        <lable>位置</lable><input type="checkbox" name="demo1_radio2" id="demo1_check" checked>
-        <label for="demo1_check">居中</label><input hidden type="range" id="demo1_pos">
+        <span>position</span>
+        <label><input type="radio" name="pos" value="start" v-model="state.position"/>start</label>
+        <label><input type="radio" name="pos" value="center" v-model="state.position"/>center</label>
+        <label><input type="radio" name="pos" value="end" v-model="state.position"/>end</label>
+      </section>
+      <section v-if="state.position!=='center'">
+        <span>offset</span>
+        <input type="range" v-model="state.offset" />{{state.offset}}
       </section>
     </aside>
   </main>
 </template>
+<script setup>
+import { reactive, computed } from 'vue'
 
+const state = reactive({ 
+  radius: 10,
+  direction: 'horizontal',
+  position: 'start',
+  offset: 20
+})
+
+const style = computed(() => {
+  const offset = state.position==='center'?'50%':state.offset+'px';
+  const position = `${state.direction === 'horizontal'?'':0} ${state.position=='end'?'':'-'}${state.radius}px`;
+  return {
+    '-webkit-mask': `radial-gradient(circle at ${state.position=='end'?'right':''} ${state.direction === 'horizontal'? state.radius+'px' : offset} ${state.position=='end'?'bottom':''} ${state.direction === 'horizontal'? offset : state.radius+'px'}, #0000 ${state.radius}px, red 0) ${position}`,
+  }
+})
+
+</script>
 <style scoped>
 h1{
     font-size: 4em;
