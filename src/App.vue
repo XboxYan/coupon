1707c7baed111c4eef54387d38db137d5d54e908
XboxYan <yanwenbin1991@live.com>
2021-04-28 06:07:20 +0000

    Deploying to gh-pages from @ XboxYan/coupon@bcc844b92712497a1f166d891ca4a7c4f473c586 🚀

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Header/>
+  <Main/>
+</template>
+
+<script setup>
+import Header from './components/header.vue'
+import Main from './components/main.vue'
+</script>
+
---
+++ src/components/main.vue
@@ -0,0 +1,9 @@
+<template>
+  <Card1/>
+</template>
+<script setup>
+import Card1 from './card1.vue'
+</script>
+<style scoped>
+
+</style>
