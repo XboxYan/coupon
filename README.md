717c483f99f4661ca45a0ca7a3f7b308e88d9352
严文彬 <yanwenbin@yuewen.com>
2021-05-06 20:29:16 +0800

    © 2021

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Generate Coupon-UI CSS code
 
 [online preview](https://coupon.codelabo.cn)
+
+In the project directory, you can run:
+
+```v
+npm install
+npm run dev
+```
+
+Then Open http://localhost:3000 to view it
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,10 +1,11 @@
 <template>
-  <footer>Made by <a href="https://github.com/XboxYan" target="_blank">xboxyan</a></footer>
+  <footer>© 2021 made by <a href="https://github.com/XboxYan" target="_blank">xboxyan</a></footer>
 </template>
 
 <style scoped>
 footer{
   padding: 2rem;
   margin-top: auto;
+  font-size: 1.2rem;
 }
 </style>
