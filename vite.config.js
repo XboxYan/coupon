723011504b4beb8c36051ef08b164199dfa97def
严文彬 <yanwenbin@yuewen.com>
2021-05-12 14:28:42 +0800

    manifest

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,28 @@ import {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),VitePWA()]
+  plugins: [vue(),VitePWA({
+    manifest: {
+      "name": "coupon.io",
+      "short_name": "coupon",
+      "start_url": ".",
+      "display": "standalone",
+      "background_color": "#fff",
+      "description": "Generate Coupon-UI CSS code",
+      "icons": [ {
+        "src": "public/favicon.svg",
+        "sizes": "72x72",
+        "type": "image/png"
+      }, {
+        "src": "public/favicon.svg",
+        "sizes": "144x144",
+        "type": "image/png"
+      }, {
+        "src": "public/favicon.svg",
+        "sizes": "192x192",
+        "type": "image/png"
+      }],
+      "theme_color": "#4fc08d"
+    },
+  })]
 })
